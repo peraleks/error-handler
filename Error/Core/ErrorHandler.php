8dabfb338adb59a8b84997540e878df1caa7dbaa
Aleksey Perevoschikov <zdorovoveselo@gmail.com>
2017-01-29 13:49:30 +0300

    ErrorHandler работа с одним ErrorObject остальные удалены

--- Error/Core/ErrorHandler.php
+++ Error/Core/ErrorHandler.php
@@ -1,11 +1,7 @@
 <?php
+declare(strict_types=1);
 
 namespace MicroMir\Error\Core;
-
-use MicroMir\Error\ErrorObjects\AbstractErrorObject;
-use MicroMir\Error\ErrorObjects\ErrorObject;
-use MicroMir\Error\ErrorObjects\ExceptionObject;
-use MicroMir\Error\ErrorObjects\ShutdownObject;
 
 class ErrorHandler
 {
@@ -32,27 +28,48 @@ class ErrorHandler
         return self::$instance;
     }
 
-    public function error()
+    public function error($code, $message, $file, $line)
     {
-        $this->handle(new ErrorObject(debug_backtrace()));
+        $this->handle(
+            new ErrorObject(
+                new \ErrorException(
+                    $message,
+                    $code,
+                    $code,
+                    $file,
+                    $line
+                ), 'error handler'
+            )
+        );
         return true;
     }
 
-    public function exception()
+    public function exception($obj)
     {
-        $this->handle(new ExceptionObject(debug_backtrace()));
+        $this->handle(new ErrorObject($obj, 'exception handler'));
     }
 
     public function shutdown()
     {
-        if (${0} = error_get_last()) $this->handle(new ShutdownObject(${0}));
+        if (${0} = error_get_last()) {
+            $this->handle(
+                new ErrorObject(
+                    new \ErrorException(
+                        ${0}['message'],
+                        ${0}['type'],
+                        ${0}['type'],
+                        ${0}['file'],
+                        ${0}['line']
+                    ), 'shutdown function'
+                )
+            );
+        }
     }
 
-    private function handle(AbstractErrorObject $obj)
+    private function handle(ErrorObject $obj)
     {
-        if (!$this->helper) {
-            $this->helper = new Helper(new SettingsObject($this->settingsFile, __CLASS__));
-        }
+        $this->helper
+            ?: $this->helper = new Helper(new SettingsObject($this->settingsFile, ""));
         $this->helper->handle($obj);
     }
 }
