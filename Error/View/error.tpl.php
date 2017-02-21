5e6e404b5778ade167fff271e287995fd3482ad1
Aleksey Perevoschikov <zdorovoveselo@gmail.com>
2017-02-22 01:21:08 +0300

    не показывать кнопку если трейс не обрабатывается

--- Error/View/error.tpl.php
+++ Error/View/error.tpl.php
@@ -1,26 +1,7 @@
+<?php $id = 'wrap'.rand(); ?>
 <style>
     <?= $style ?>
 </style>
-<script>
-    window.onload = function () {
-        var trace = document.querySelectorAll('.but_trace');
-        for (var i = 0; i < trace.length; i++) {
-            trace[i].onclick = function () {
-                this.parentNode.parentNode.children[2].classList.toggle('hidden');
-            };
-        }
-        var wraps = document.querySelectorAll('.trace_wrap');
-        for (var i = 0; i < wraps.length; i++) {
-            wraps[i].addEventListener('click', function (e) {
-                    var target = e.target.querySelector('.tooltip_wrap');
-                    if (null != target) {
-                        target.classList.toggle('hidden');
-                    }
-                }
-            );
-        }
-    }
-</script>
 <div class="error_box" style="font-size: <?= $fontSize ?>px">
     <div class="<?= $cssName ?> error_header">
         <?= '['.$code.'] '.$name ?>
@@ -29,13 +10,30 @@
     <div class="error_text error_content">
         <?= $message ?>
     </div>
-    <div class="trace_wrap <?= $hidden ?>">
+    <div id="<?= $id ?>" class="trace_wrap <?= $hidden ?>">
         <?= $trace ?>
     </div>
     <div class="error_path error_content">
-        <div class="but_trace">trace</div>
+        <?php if ($trace != ''): ?>
+        <div class="but_trace" onclick = "parentNode.previousElementSibling.classList.toggle('hidden')">
+            trace
+        </div>
+        <?php endif; ?>
         <?= $file ?>
         <div class="error_path error_content error_line"><?= $line ?></div>
         <span class="app_dir"><?= $path ?></span>
     </div>
 </div>
+<?php if ($trace != ''): ?>
+<script>
+    (function () {
+        var wrap = document.getElementById('<?= $id ?>');
+        wrap.addEventListener('click', function (e) {
+            var target = e.target.querySelector('.tooltip_wrap');
+            if (null != target) {
+                target.classList.toggle('hidden');
+            }
+        })
+    })();
+</script>
+<?php endif; ?>
