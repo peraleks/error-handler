07a1bc6839a5b1f3c1155384a35746e57da6e5af
Aleksey Perevoschikov <zdorovoveselo@gmail.com>
2017-02-27 20:10:47 +0300

    новый функционал конфигурации

--- src/config.php
+++ src/config.php
@@ -9,36 +9,40 @@ return [
 
     'DEVELOPMENT_MODE_CONSTANT' => 'DEVELOPMENT_MODE',
 
-    'APP_DIR' => $_SERVER['DOCUMENT_ROOT'],
+//    'APP_DIR' => dirname(__DIR__),
 
     'DEV' => [
         \Peraleks\ErrorHandler\Notifiers\HtmlNotifier::class => [
-            'enabledFor'     => E_ALL,
-            'deferredView'   => true,
-            'hideView'       => true,
-            'handleTraceFor' => TRACE,
-            'hideTrace'      => true,
-            'fontSize'       => 15,
-            'stringLength'   => 80,
-            'tooltipLength'  => 1000,
-            'arrayLevel'     => 2,
+            'enabled'       => E_ALL,
+            'deferredView'  => true,
+            'hideView'      => true,
+            'handleTrace'   => TRACE,
+            'hideTrace'     => true,
+            'fontSize'      => 15,
+            'stringLength'  => 80,
+            'tooltipLength' => 1000,
+            'arrayLevel'    => 2,
         ],
         \Peraleks\ErrorHandler\Notifiers\TailNotifier::class => [
-//            'enabledFor'  => E_ALL,
+            'enabled'     => E_ALL,
             'handleTrace' => TRACE,
+            'simpleTrace' => true,
             'file'        => $_SERVER['DOCUMENT_ROOT'].'/tail_error.log',
         ],
+    ],
+
+    'CLI' => [
+        \Peraleks\ErrorHandler\Notifiers\CliNotifier::class => [
+            'enabled'     => E_ALL,
+            'handleTrace' => TRACE,
+            'simpleTrace' => true,
+        ],
+
     ],
 
     'PROD' => [
 
     ],
 
-    'CLI' => [
-        \Peraleks\ErrorHandler\Notifiers\CliNotifier::class => [
-            'enabledFor' => E_ALL,
-//            'handleTrace'   => TRACE,
-        ],
 
-    ],
 ];
