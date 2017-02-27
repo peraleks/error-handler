e18d485a42c573151d0390f9cd7c35e5e4721327
Aleksey Perevoschikov <zdorovoveselo@gmail.com>
2017-02-27 20:03:49 +0300

    переработан шаблонный метод. Добавлен renderTrace

--- src/Notifiers/AbstractNotifier.php
+++ src/Notifiers/AbstractNotifier.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Peraleks\ErrorHandler\Notifiers;
 
-use Peraleks\ErrorHandler\Core\SettingsInterface;
+use Peraleks\ErrorHandler\Core\ConfigInterface;
 use Peraleks\ErrorHandler\Core\ErrorObject;
 use Peraleks\ErrorHandler\Core\ShutdownCallbackInterface;
 
@@ -11,23 +11,39 @@ abstract class AbstractNotifier
 {
     protected $errorObject;
 
-    protected $settingsObject;
+    protected $configObject;
 
     protected $errorHandler;
 
+    protected $traceHandlerClass;
+
+    protected $renderedError;
+
     public function __construct(
         ErrorObject $errorObject,
-        SettingsInterface $settingsObject,
+        ConfigInterface $configObject,
         ShutdownCallbackInterface $errorHandler
     ) {
     
         $this->errorObject = $errorObject;
-        $this->settingsObject = $settingsObject;
+        $this->configObject = $configObject;
         $this->errorHandler = $errorHandler;
         $this->prepare();
+        $this->renderedError = $this->renderError($this->renderTrace($this->traceHandlerClass));
     }
 
     abstract protected function prepare();
 
     abstract public function notify();
+
+    abstract protected function renderError(string $trace): string;
+
+    protected function renderTrace(string $handlerClass): string
+    {
+        if (0 != ($this->configObject->get('handleTrace') & $this->errorObject->getCode())) {
+            $handler = new $handlerClass($this->errorObject->getTrace(), $this->configObject);
+            return  $handler->getTrace();
+        }
+        return '';
+    }
 }
