f88b956167712c1e423871db2e838ed8d7d22cb0
Aleksey Perevoschikov <aleksey.perevoshchikov.n@gmail.com>
2017-03-06 03:18:36 +0300

    неболшие доработки сонфигурации

--- src/Config/main.php
+++ src/Config/main.php
@@ -4,38 +4,41 @@ define('DEVELOPMENT_MODE', true);
 
 $modeConstant = 'DEVELOPMENT_MODE';
 
-
 if (PHP_SAPI === 'cli') {
     $config = __DIR__.'/configCLI.php';
 } elseif (defined($modeConstant) && true === constant($modeConstant)) {
     $config = __DIR__.'/configDEV.php';
+    $mode = 'dev';
 } else {
     $config = __DIR__.'/configPROD.php';
 }
 
-
 $trace = E_ERROR | E_RECOVERABLE_ERROR;
 
 return [
+    'SELF_LOG_FILE'   => '',
     'ERROR_REPORTING' => E_ALL,
-    'APP_DIR'         => dirname(dirname(__DIR__)),
+    'APP_DIR'         => 'default',
     'NOTIFIERS'       => require $config,
+    'MODE'            => $mode ?? 'prod',
 ];
 
-/*
-E_ERROR
-E_WARNING
-E_PARSE
-E_NOTICE
-E_CORE_ERROR
-E_CORE_WARNING
-E_COMPILE_ERROR
-E_COMPILE_WARNING
-E_USER_ERROR
-E_USER_WARNING
-E_USER_NOTICE
-E_STRICT
-E_RECOVERABLE_ERROR
-E_DEPRECATED
-E_USER_DEPRECATED
+
+/*  0
+    1 E_ERROR
+    2 E_WARNING
+    4 E_PARSE
+    8 E_NOTICE
+   16 E_CORE_ERROR
+   32 E_CORE_WARNING
+   64 E_COMPILE_ERROR
+  128 E_COMPILE_WARNING
+  256 E_USER_ERROR
+  512 E_USER_WARNING
+ 1024 E_USER_NOTICE
+ 2048 E_STRICT
+ 4096 E_RECOVERABLE_ERROR
+ 8192 E_DEPRECATED
+16384 E_USER_DEPRECATED
+32767 E_ALL
 */
