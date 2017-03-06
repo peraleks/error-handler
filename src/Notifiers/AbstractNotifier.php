a336f11645b04479320a4dcdb47cc6cb4f5b75c6
Aleksey Perevoschikov <aleksey.perevoshchikov.n@gmail.com>
2017-03-06 03:05:13 +0300

    изменён шаблонный метод (+ getTraceHandlerClass() )

--- src/Notifiers/AbstractNotifier.php
+++ src/Notifiers/AbstractNotifier.php
@@ -15,8 +15,6 @@ abstract class AbstractNotifier
 
     protected $errorHandler;
 
-    protected $traceHandlerClass = '';
-
     protected $finalStringError;
 
     public function __construct(
@@ -29,27 +27,29 @@ abstract class AbstractNotifier
         $this->configObject = $configObject;
         $this->errorHandler = $errorHandler;
         $this->prepare();
-        $this->finalStringError = $this->ErrorToString($this->TraceToString($this->traceHandlerClass));
+        $this->finalStringError = $this->ErrorToString($this->TraceToString($this->getTraceHandlerClass()));
     }
 
     abstract protected function prepare();
+
+    abstract protected function getTraceHandlerClass(): string;
 
     abstract public function notify();
 
     abstract protected function ErrorToString(string $trace): string;
 
-    protected function TraceToString(string $handlerClass): string
+    protected function TraceToString(string $traceHandlerClass): string
     {
         $err = $this->errorObject;
         $con = $this->configObject;
 
-        if ('' == $handlerClass) return '';
+        if ('' == $traceHandlerClass) return '';
 
         if (0 != ($con->get('handleTrace') & $err->getCode())) {
 
             if ($con->get('phpNativeTrace')) return $err->getTraceAsString();
 
-            $handler = new $handlerClass($err->getTrace(), $con);
+            $handler = new $traceHandlerClass($err->getTrace(), $con);
             return  $handler->getTrace();
         }
         return '';
