8777a7483cfe3627600576fe93226e5356867aa3
Aleksey Perevoschikov <aleksey.perevoshchikov.n@gmail.com>
2017-03-09 00:13:50 +0300

    Доработан внутренний обработчик ошибок

--- src/Core/SelfErrorHandler.php
+++ src/Core/SelfErrorHandler.php
@@ -28,19 +28,32 @@ class SelfErrorHandler
 
     private $devMode;
 
+    private $code;
+
+    private $traceEnabled = E_ERROR | E_RECOVERABLE_ERROR;
+
+    private $error500 = E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR;
+
     public function __construct(ConfigSelfErrorInterface $configObject = null)
     {
         if ($configObject && ('' !== $configObject->getSelfLogFile())) {
             $this->selfLogFile = $configObject->getSelfLogFile();
-        } else {
+        } elseif (PHP_SAPI !== 'cli') {
             $this->selfLogFile = $_SERVER['DOCUMENT_ROOT'].'/error_handler_'
                 .crc32($_SERVER['DOCUMENT_ROOT'].$_SERVER['SERVER_SOFTWARE']).'.log';
         }
-        $this->devMode = $configObject && ('dev' == $configObject->getMode());
+        $this->devMode = $configObject && ('dev' === $configObject->getMode());
     }
 
-    public function report(\Throwable $e)
+    public function report($e)
     {
+
+        if (!$e instanceof \ErrorException && !$e instanceof ErrorObject) {
+            $this->code = $e instanceof \ParseError ? E_PARSE : E_ERROR;
+        } else {
+            $this->code = $e->getCode();
+        }
+
         if (PHP_SAPI === 'cli') {
             $this->cliReport($e);
             return;
@@ -55,29 +68,63 @@ class SelfErrorHandler
 
     private function cliReport($e)
     {
-        echo "\033[32m".(string)$e."\033[0m";
+        echo "\n\033[32m".$this->getStringError($e)."\033[0m\n";
     }
 
     private function htmlReport($e)
     {
-        if ($e instanceof \ErrorException) {
-            $type = $this->codeName[$e->getCode()] ?? 'unknown';
-        } else {
-            $type = get_class($e);
-        }
+        $type    = $this->getType($e);
         $file    = $e->getFile();
         $line    = $e->getLine();
         $message = $e->getMessage();
-        $trace   = '<pre>'.$e->getTraceAsString().'</pre>';
+        $trace   = $this->code & $this->traceEnabled ? '<pre>'.$e->getTraceAsString().'</pre>' : '';
 
         include dirname(__DIR__).'/View/selfError.tpl.php';
     }
 
     private function fileReport($e, string $file)
     {
-        if ($fileResource = fopen($file, 'ab')) {
-            fwrite($fileResource, "\n[".date('d-M-o H:i:s O').'] '.(string)$e."\n");
-            fclose($fileResource);
+        if ($r = fopen($file, 'ab')) {
+            fwrite($r, "\n[".date('d-M-o H:i:s O').'] '.$this->getStringError($e)."\n");
+            fclose($r);
+        }
+
+        /* Если $e->getCode() вернёт 0 (\Throwable), значит ошибка сгенерирована
+         * внутри обработчика и была перехвачена - поэтому скрипт не останавливаем
+         * и не посылаем состояние 500.
+         * Если $e является экземпляром ErrorObject, значит ошибка была в клиентской
+         * части кода. ErrorObject конвертирует коды ошибок и никогда не возвращает 0.
+         * В этом случае при совпадении кода с маской $this->error500 и в режиме production
+         * посылаем состояние 500 и останавливаем скрипт.
+         * Если фатальная ошибка произошла внутри обработчика, аналогично - 500 и exit */
+        if (!$this->devMode && ($e->getCode() & $this->error500)) {
+            $this->clean();
+            headers_sent() ?: header('HTTP/1.1 500 Internal Server Error');
+            include dirname(__DIR__).'/View/serverError500.php';
+            exit;
         }
     }
+
+    private function clean()
+    {
+        ob_end_clean();
+        if (0 < ob_get_level()) $this->clean();
+    }
+
+    private function getType($e): string
+    {
+        if ($e instanceof \ErrorException) {
+            return $this->codeName[$e->getCode()] ?? 'unknown';
+        }
+        return get_class($e);
+    }
+
+    private function getStringError($e): string
+    {
+        if (!($this->code & $this->traceEnabled)) {
+            return $this->getType($e).': '.$e->getMessage().' in '.$e->getFile().':'.$e->getLine();
+        }
+        return (string)$e;
+    }
+
 }
